ed5bc8a914f22d3019218e9b4836344fedfd542c
Eoin Mulvey <c14752305@mydit.ie>
2016-04-07 23:50:50 +0100

    Adding rhombus and parallelogram classes

---
+++ src/com/project/OOP2/DrawParallelogram.java
@@ -0,0 +1,36 @@
+/*
+ * Author: C14752305 
+ * Class: DrawParallelogram
+ * Date: 7/04/2016
+ * Handles how parallelograms are drawn.
+ */
+package com.project.OOP2;
+
+import java.awt.geom.Path2D;
+
+public class DrawParallelogram {
+	float x1, x2, y1, y2;
+	Path2D.Float drawParallelogram;
+	
+	public DrawParallelogram(float x1, float x2, float y1, float y2){
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y2;
+		this.y2 = y2;
+		//Sort positions
+		float xa = Math.min(x1, x2);
+		float xb = Math.max(x1, x2);
+		//Set adjusted positions
+		float xc = xa + 20;
+		float xd = xb + 20;
+				
+		//Draw parallelogram
+		this.drawParallelogram = new Path2D.Float();
+		drawParallelogram.moveTo(xa, y1);
+		drawParallelogram.lineTo(xb, y1);
+		drawParallelogram.lineTo(xd, y2);
+		drawParallelogram.lineTo(xc, y2);
+		drawParallelogram.lineTo(xa, y1);
+		drawParallelogram.closePath();
+	}
+}
---
+++ src/com/project/OOP2/DrawRhombus.java
@@ -0,0 +1,32 @@
+/*
+ * Author: C14752305 
+ * Class: DrawRhombus
+ * Date: 7/04/2016
+ * Handles how diamonds are drawn.
+ */
+package com.project.OOP2;
+
+import java.awt.geom.Path2D;
+
+public class DrawRhombus {
+	float x1, x2, y1, y2;
+	Path2D.Float drawRhombus;
+	
+	public DrawRhombus(float x1, float x2, float y1, float y2){
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y2;
+		this.y2 = y2;
+		//Get middle position
+		float x3 = (x1 + x2)/2;
+		float y3 = (y1 + y2)/2;
+		//Draw rhombus shape
+		this.drawRhombus = new Path2D.Float();
+		drawRhombus.moveTo(x1, y3);
+		drawRhombus.lineTo(x3, y1);
+		drawRhombus.lineTo(x2, y3);
+		drawRhombus.lineTo(x3, y2);
+		drawRhombus.lineTo(x1, y3);
+		drawRhombus.closePath();
+	}
+}
